2b9e2532650dc73343ccb0c1a8da47a88943c0ef
Olakunle Salami <kunsal2003@gmail.com>
2019-11-26 22:52:44 +0100

    Updates

--- README.md
+++
@@ -1,4 +0,0 @@
-[![Build Status](https://travis-ci.com/kunsal/teamwork-frontend.svg?branch=develop)](https://travis-ci.com/kunsal/teamwork-frontend)
-
-# Teamwork Frontend
-Employee social engagement application for sharing articles and gifs.
---
+++ precache-manifest.7bcf128c9994786dbdf90fd3d5220574.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85afe8b7583ce6d7d9b925832ed388a9",
+    "url": "/teamwork-frontend/index.html"
+  },
+  {
+    "revision": "efee2e501fd6a247b4f5",
+    "url": "/teamwork-frontend/static/css/2.0c2e8eb3.chunk.css"
+  },
+  {
+    "revision": "f0aa3f32354791861935",
+    "url": "/teamwork-frontend/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "efee2e501fd6a247b4f5",
+    "url": "/teamwork-frontend/static/js/2.762f3154.chunk.js"
+  },
+  {
+    "revision": "f0aa3f32354791861935",
+    "url": "/teamwork-frontend/static/js/main.e112b999.chunk.js"
+  },
+  {
+    "revision": "9030b45cb4529a6be40f",
+    "url": "/teamwork-frontend/static/js/runtime-main.621c0826.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/teamwork-frontend/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/teamwork-frontend/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/teamwork-frontend/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/teamwork-frontend/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/teamwork-frontend/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/teamwork-frontend/static/media/logo.25bf045c.svg"
+  }
+]);
